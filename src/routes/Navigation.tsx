6cd41bace7cd70a25fc873e6a7c6ed7d91961d0b
Juan Guana <juanc.guana@gmail.com>
2022-02-09 20:32:03 -0500

    feat: :sparkles: Add routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../index.css';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 export const Navigation = () => {
   return (
@@ -17,35 +18,35 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to='/home'
+                to='/lazy1'
                 className={({ isActive }) => (isActive ? 'nav-active' : '')}
               >
-                Home
+                Lazy1
               </NavLink>
             </li>
             <li>
               <NavLink
-                to='/about'
+                to='/lazy2'
                 className={({ isActive }) => (isActive ? 'nav-active' : '')}
               >
-                About
+                Lazy2
               </NavLink>
             </li>
             <li>
               <NavLink
-                to='/users'
+                to='/lazy3'
                 className={({ isActive }) => (isActive ? 'nav-active' : '')}
               >
-                Users
+                Lazy3
               </NavLink>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path='about' element={<h1>About Page</h1>} />
-          <Route path='users' element={<h1>Users Page</h1>} />
-          <Route path='home' element={<h1>Home Page</h1>} />
-          <Route path='/*' element={<Navigate to='/home' replace />} />
+          <Route path='lazy1' element={<LazyPage1 />} />
+          <Route path='lazy2' element={<LazyPage2 />} />
+          <Route path='lazy3' element={<LazyPage3 />} />
+          <Route path='/*' element={<Navigate to='/lazy1' replace />} />
         </Routes>
       </div>
     </BrowserRouter>
