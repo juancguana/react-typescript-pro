342391a1e33521844b5aacfece632ff68b1455bc
Juan Guana <juanc.guana@gmail.com>
2023-05-04 16:29:20 -0500

    feat: Control props alt

--- src/02-components-patterns/hooks/useProduct.tsx
+++ src/02-components-patterns/hooks/useProduct.tsx
@@ -14,12 +14,7 @@ export const useProduct = ({
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
